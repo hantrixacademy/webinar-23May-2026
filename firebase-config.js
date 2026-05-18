2b0d444bf725f47a827029f0a26c590b5d51af97
Ashfaq Anil <ashfaqanil@Ashfaqs-MacBook-Air.local>
2026-05-18 08:27:37 +0530

    Initial deploy

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCOoe5fmxmvzpte0mkvUjifItuTRsOlxYE",
+  authDomain:        "webinar-23-2026.firebaseapp.com",
+  projectId:         "webinar-23-2026",
+  storageBucket:     "webinar-23-2026.firebasestorage.app",
+  messagingSenderId: "270587833411",
+  appId:             "1:270587833411:web:920505d91532abb6f738f6",
+  measurementId:     "G-KSSPHW65R3"
+};
---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL      = 'https://wjcnedcowbiidmqvcxrt.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_-ScUT2fuUCjQTp6FbAANgw_ZfYLnI26';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
